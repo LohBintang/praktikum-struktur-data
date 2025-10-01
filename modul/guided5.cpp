5da05d30dd37176e0c124c41469ea691c9a8b514
LohBintang <bintang140299@gmail.com>
2025-10-01 10:41:18 +0700

    Add files via upload

---
+++ modul/guided5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+using namespace std;
+
+int main() {
+    cout << "Nama : Bintang \nNIM : 103112400116" << endl;
+
+    int nomor = 1;
+    cout << nomor << endl;
+    cout << nomor++ << endl;
+    cout << ++nomor << endl;
+    return 0;
+
+}
