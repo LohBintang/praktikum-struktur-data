d5505cb0e85584b2c6f845dbf1c8a53b4c3556c4
LohBintang <bintang140299@gmail.com>
2025-11-04 23:04:17 +0700

    Add files via upload

---
+++ modul7/soal1/stack.h
@@ -0,0 +1,19 @@
+#ifndef STACK_H
+#define STACK_H
+
+const int maxEl = 20;
+
+typedef int infotype;
+
+struct Stack {
+    infotype info[maxEl];
+    int top;
+};
+
+void createStack(Stack &S);
+void push(Stack &S, infotype x);
+infotype pop(Stack &S);
+void printInfo(const Stack &S);
+void balikStack(Stack &S);
+
+#endif
