43e30a95a8b1e6b08eb9699e526be39e77bbef7f
LohBintang <bintang140299@gmail.com>
2025-12-03 21:30:13 +0700

    Add files via upload

---
+++ modul10/soal3/bstree.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include "bstree.h"
+using namespace std;
+
+address alokasi(infotype x) {
+    address p = new Node;
+    p->info = x;
+    p->left = NULL;
+    p->right = NULL;
+    return p;
+}
+
+void insertNode(address &root, infotype x) {
+    if (root == NULL) {
+        root = alokasi(x);
+    } else if (x < root->info) {
+        insertNode(root->left, x);
+    } else if (x > root->info) {
+        insertNode(root->right, x);
+    }
+}
+
+address findNode(infotype x, address root) {
+    if (root == NULL) return NULL;
+    if (x == root->info) return root;
+    else if (x < root->info) return findNode(x, root->left);
+    else return findNode(x, root->right);
+}
+
+void printInOrder(address root) {
+    if (root != NULL) {
+        printInOrder(root->left);
+        cout << root->info << " - ";
+        printInOrder(root->right);
+    }
+}
+
+void printPreOrder(address root) {
+    if (root != NULL) {
+        cout << root->info << " - ";
+        printPreOrder(root->left);
+        printPreOrder(root->right);
+    }
+}
+
+void printPostOrder(address root) {
+    if (root != NULL) {
+        printPostOrder(root->left);
+        printPostOrder(root->right);
+        cout << root->info << " - ";
+    }
+}
+
+
+int hitungJumlahNode(address root) {
+    if (root == NULL)
+        return 0;
+    return 1 + hitungJumlahNode(root->left) + hitungJumlahNode(root->right);
+}
+
+int hitungTotalInfo(address root, int start) {
+    if (root == NULL)
+        return start;
+    return root->info 
+         + hitungTotalInfo(root->left, 0)
+         + hitungTotalInfo(root->right, 0);
+}
+
+int hitungKedalaman(address root, int start) {
+    if (root == NULL)
+        return start;
+
+    int leftDepth = hitungKedalaman(root->left, start + 1);
+    int rightDepth = hitungKedalaman(root->right, start + 1);
+
+    return (leftDepth > rightDepth ? leftDepth : rightDepth);
+}
